341d52d5682895ae58e2f8758521b463e98ae55e
Bertrand Bourion <bertrandbou@hotmail.fr>
2021-06-06 09:42:38 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld msg="Bienvenue a parc sud"/>
 </template>
 
 <script>
@@ -15,12 +14,21 @@ export default {
 </script>
 
 <style>
+
+body{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100vh;
+  overflow: hidden;
 }
 </style>
